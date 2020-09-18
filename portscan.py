bf60fd2730fe4a2232e11bd355ead4e544375f79
PhilPore <47266383+PhilPore@users.noreply.github.com>
2020-09-18 13:10:59 -0400

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -5,7 +5,7 @@ import socket
 import sys
 
 
-def TCP_scan():
+def TCP_scan(): 
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def UDP_scan():
@@ -15,8 +15,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Scans a host for open ports")
     parser.add_argument("host", help="IPv4 address of host", type=str)
     parser.add_argument("scantype", help="U - UDP, T - TCP", type=str)
+    #need to add another parser arg to check to see if all ports or famous ports need to be checked.
     args = parser.parse_args()
     
+    
+    #move to either TCP or UDP functions. Will need to pass parameters.
     if args.scantype == 'T':
         print("10")
         TCP_scan()
