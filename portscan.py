60a714b4b65a7f1c8217fa321ea9e65fd9713fb3
PhilPore <47266383+PhilPore@users.noreply.github.com>
2020-09-18 13:08:29 -0400

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -4,11 +4,27 @@ import argparse
 import socket
 import sys
 
+
+def TCP_scan():
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def UDP_scan():
+    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Scans a host for open ports")
     parser.add_argument("host", help="IPv4 address of host", type=str)
+    parser.add_argument("scantype", help="U - UDP, T - TCP", type=str)
     args = parser.parse_args()
+    
+    if args.scantype == 'T':
+        print("10")
+        TCP_scan()
+    else:
+        print("01")
+        UDP_scan()
 
-    udpsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+    #udpsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
     print(args)
