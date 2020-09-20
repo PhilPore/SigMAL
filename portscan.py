555f026baddb9646dd0488e83019dade4996c24f
peppermintpatty5 <peppermintpatty5.scratch@gmail.com>
2020-09-20 17:24:35 -0400

    Tried to some cool stuff with ICMP echo and TCP syn

--- portscan.py
+++ portscan.py
@@ -2,29 +2,55 @@
 
 import argparse
 import socket
+import struct
 import sys
 
+availableports = []
 
-def TCP_scan():
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def UDP_scan():
-    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def TCP_scan(host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1.0)
+
+    try:
+        sock.connect((host, port))
+        availableports.append(port)
+    except:
+        print("Connection failed!")
+
+
+def UDP_scan(host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(1.0)
+
+    try:
+        sock.sendto(struct.pack("!i", 69), (host, port))
+        data, addr = sock.recvfrom(64)
+        print(data)
+        sock.s
+    except:
+        print("Connection failed!")
+
+
+def ICMP_ping(host):
+    pass
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Scans a host for open ports")
     parser.add_argument("host", help="IPv4 address of host", type=str)
-    parser.add_argument("scantype", help="U - UDP, T - TCP", type=str)
+    parser.add_argument("port", help="Destination port", type=int)
+    parser.add_argument(
+        "scantype", help="Type of scan to perform", choices=["U", "T", "I"], type=str
+    )
     args = parser.parse_args()
-    
-    if args.scantype == 'T':
-        print("10")
-        TCP_scan()
+
+    if args.scantype == "T":
+        TCP_scan(args.host, 1)
     else:
-        print("01")
-        UDP_scan()
+        # for port in range(1 << 10):
+        UDP_scan(args.host, 80)
 
+    # udpsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    #udpsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    
     print(args)
